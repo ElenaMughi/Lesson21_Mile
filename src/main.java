82c90a0637c16068e02a2c619a2a44934e9bece3
ElenaMughi <elena.mughi@gmail.com>
2021-11-10 23:41:27 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+    int  TicketCost = 4589;
+    int BonusMile;
+    BonusMile = TicketCost/20;
+    System.out.println(BonusMile);
+    }
+}
