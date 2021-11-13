f6b8a8ecbe49a3635546b2ddd73b3c2f9fb82a34
ElenaMughi <elena.mughi@gmail.com>
2021-11-13 15:17:43 +0300

    Fix var

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    int ticketcost = 4599;
+    int bonusmile;
+    bonusmile = ticketcost/20;
+    System.out.println(bonusmile);
+    }
+}
--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-    int  TicketCost = 4589;
-    int BonusMile;
-    BonusMile = TicketCost/20;
-    System.out.println(BonusMile);
-    }
-}
